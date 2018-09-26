07d5024ca00e655844f97b301492ff0b0e9df8a0
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-09-26 14:14:47 -0500

    Scripted auto-commit on change (2018-09-26 14:14:47) by gitwatch.sh

--- locking-etc/BankAccountFix1.java
+++ locking-etc/BankAccountFix1.java
@@ -1,19 +1,19 @@
 public class BankAccount {
     private int balance;
-    private boolean busy = false;
+    private Object bankLock = new Object();
 
     public void setBalance(int balance) {
-        this.balance = balance;
+        synchronized (bankLock) {
+            this.balance = balance;
+        }
     }
 
     public void withdraw(int amount) {
-        while (busy) {};
-
-        busy = true;
-        int b = balance;
-        int a = amount;
-        int ans = b - a;
-        balance = ans;
-        busy = false;
+        synchronized (bankLock) {
+            int b = balance;
+            int a = amount;
+            int ans = b - a;
+            balance = ans;
+        }
     }
 }
