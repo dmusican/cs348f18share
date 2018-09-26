68f7742c8b1a3794a33cce7e9d3e8cbdecf20779
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-09-26 13:50:05 -0500

    Scripted auto-commit on change (2018-09-26 13:50:05) by gitwatch.sh

--- locking-etc/BankAccount.java
+++ locking-etc/BankAccount.java
@@ -1,3 +1,11 @@
 public class BankAccount {
-    private int balance
-    
+    private int balance;
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
+}
