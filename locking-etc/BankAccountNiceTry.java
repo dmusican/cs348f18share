f4c329e405be565c18e9aba2ca31ffd97c715961
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-09-26 14:10:52 -0500

    Scripted auto-commit on change (2018-09-26 14:10:52) by gitwatch.sh

--- locking-etc/BankAccountNiceTry.java
+++ locking-etc/BankAccountNiceTry.java
@@ -1,15 +1,19 @@
 public class BankAccount {
     private int balance;
+    private boolean busy = false;
 
     public void setBalance(int balance) {
         this.balance = balance;
     }
 
     public void withdraw(int amount) {
-        //balance = balance - amount;
+        while (busy) {};
+
+        busy = true;
         int b = balance;
         int a = amount;
         int ans = b - a;
         balance = ans;
+        busy = false;
     }
 }
