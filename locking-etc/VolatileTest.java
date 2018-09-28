aad7899f2159612a30cf9a87e145c8cf9c6fd271
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-09-28 14:43:36 -0500

    Scripted auto-commit on change (2018-09-28 14:43:36) by gitwatch.sh

---
+++ locking-etc/VolatileTest.java
@@ -0,0 +1,42 @@
+// https://dzone.com/articles/java-volatile-keyword0-
+public class VolatileTest {
+
+    private static int sharedValue = 0;
+
+    public static void main(String[] args) {
+        new Reader().start();
+        new Incrementer().start();
+    }
+
+    public static class Reader extends Thread {
+        @Override
+        public void run() {
+            int localValue = sharedValue;
+            while (true){
+                if( localValue != sharedValue){
+                    System.out.println(
+                        "Read new sharedValue : "+
+                        sharedValue);
+                    localValue = sharedValue;
+                }
+            }
+        }
+    }
+
+    public static class Incrementer extends Thread{
+        @Override
+        public void run() {
+            int localValue = sharedValue;
+            while (true){
+                localValue++;
+                System.out.println(
+                    "Incrementing sharedValue to " +
+                    localValue);
+                sharedValue = localValue;
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) { e.printStackTrace(); }
+            }
+        }
+    }
+}
