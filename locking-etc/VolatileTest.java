d8cb76dd7aa137837e5aa51f2a8c14281064c068
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-09-28 14:54:27 -0500

    Scripted auto-commit on change (2018-09-28 14:54:27) by gitwatch.sh

--- locking-etc/VolatileTest.java
+++ locking-etc/VolatileTest.java
@@ -13,7 +13,6 @@ public class VolatileTest {
         public void run() {
             int localValue = sharedValue;
             while (true){
-                System.out.println("yo");
                 if( localValue != sharedValue){
                     System.out.println(
                         "Read new sharedValue : "+
