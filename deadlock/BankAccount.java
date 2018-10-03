7c6ad55deb5030780e3a0c373c36f83f26678081
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-10-03 13:54:35 -0500

    Scripted auto-commit on change (2018-10-03 13:54:35) by gitwatch.sh

---
+++ deadlock/BankAccount.java
@@ -0,0 +1,47 @@
+class BankAccount {
+    private int balance;
+
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+    
+    synchronized void withdraw(int amt) { // line A
+        balance -= amt;                   // line B
+    };                                    // line C
+
+    synchronized void deposit(int amt) {  // line D
+        balance += amt;                   // line E
+    }                                     // line F
+
+    synchronized void transferTo(int amt,
+                                 BankAccount other) {
+        this.withdraw(amt);
+        other.deposit(amt);                                   
+    }
+
+    public static void main(String[] args) {
+        BankAccount one = new BankAccount(1000);
+        BankAccount two = new BankAccount(2000);
+
+        Thread t1 = new Thread() {
+            public void run() {
+                while (true) {
+                    one.transferTo(500,two);
+                    System.out.println("Transfering 1");
+                }
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                while (true) {
+                    two.transferTo(100,one);
+                    System.out.println("Transfering 2");
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
+}
