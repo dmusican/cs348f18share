70f2656bfcf2f650b1ebc302f8f85e795c20d6ea
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-10-03 14:35:59 -0500

    Scripted auto-commit on change (2018-10-03 14:35:59) by gitwatch.sh

--- deadlock/BankAccount3.java
+++ deadlock/BankAccount3.java
@@ -5,7 +5,7 @@ class BankAccount3 {
     private int balance;
     private int id; 
 
-    public BankAccount3(int balance) {
+    public synchronized BankAccount3(int balance) {
         this.balance = balance;
         id = nextId.getAndIncrement();
     }
