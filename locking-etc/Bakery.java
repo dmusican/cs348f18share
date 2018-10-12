5853a385c8e9ff6af04c667a7e0eb3109a952623
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-10-12 13:31:29 -0500

    Scripted auto-commit on change (2018-10-12 13:31:29) by gitwatch.sh

--- locking-etc/Bakery.java
+++ locking-etc/Bakery.java
@@ -1,25 +1,25 @@
 class Bakery {
-  public static final int n = 10;   // or whatever
-  private boolean[] flag = new boolean[n];
-  private long[] label = new long[n];
+    public static final int n = 10;   // or whatever
+    private boolean[] flag = new boolean[n];
+    private long[] label = new long[n];
 
-  public void lock() {
-    int i = ThreadID.get();
-    flag[i] = true;
-    label[i] = max(label[...]) + 1;    // max of array
-    boolean waiting = true;
-    while (waiting) {
-        waiting = false;          // I wanna go! Can I?
-        for (k=0; k < n; k++) {
-              if (k != i && flag[k] && (label[k], k) << (label[i],i)) {
-                  // pseudocode tuple <
-                 waiting = true;
-              }
+    public void lock() {
+        int i = ThreadID.get();
+        flag[i] = true;
+        label[i] = max(label[...]) + 1;    // max of array
+        boolean waiting = true;
+        while (waiting) {
+            waiting = false;          // I wanna go! Can I?
+            for (k=0; k < n; k++) {
+                if (k != i && flag[k] &&
+                    (label[k], k) << (label[i],i)) {
+                    waiting = true;
+                }
+            }
         }
     }
-  }
 
-  public void unlock() {
-    flag[ThreadID.get()] = false;
-  }
+    public void unlock() {
+        flag[ThreadID.get()] = false;
+    }
 }
