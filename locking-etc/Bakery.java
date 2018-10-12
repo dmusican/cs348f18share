793c0bbb99ec6fbaca35a0ecdfb8560a0214c39a
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-10-12 13:30:07 -0500

    Scripted auto-commit on change (2018-10-12 13:30:07) by gitwatch.sh

---
+++ locking-etc/Bakery.java
@@ -0,0 +1,25 @@
+class Bakery {
+  public static final int n = 10;   // or whatever
+  private boolean[] flag = new boolean[n];
+  private long[] label = new long[n];
+
+  public void lock() {
+    int i = ThreadID.get();
+    flag[i] = true;
+    label[i] = max(label[...]) + 1;    // I assume we know what max must do
+    boolean waiting = true;
+    while (waiting) {
+        waiting = false;     // can I go? please?
+        for (k=0; k < n; k++) {    // && !waiting if you like
+              if (k != i && flag[k] && (label[k], k) << (label[i],i)) {
+                  // pseudocode tuple <
+                 waiting = true;
+              }
+        }
+    }
+  }
+
+  public void unlock() {
+    flag[ThreadID.get()] = false;
+  }
+}
