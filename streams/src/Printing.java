814dd51193d1f2b956143b08357ff3207ed55802
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-10-19 15:04:40 -0500

    Scripted auto-commit on change (2018-10-19 15:04:40) by gitwatch.sh

--- streams/src/Printing.java
+++ streams/src/Printing.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Printing {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>();
+        int size = 1000;
+        for (int i=0; i < size; i++) {
+            myList.add(i);
+        }
+
+        myList.stream().forEach(item -> System.out.println(item));
+        
+
+    }
 }
