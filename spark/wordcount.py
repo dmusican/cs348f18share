eca44529c94fbcff3ddb68fb08eb3c78a2223539
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-11-05 14:04:27 -0600

    Scripted auto-commit on change (2018-11-05 14:04:27) by gitwatch.sh

--- spark/wordcount.py
+++ spark/wordcount.py
@@ -1,5 +1,7 @@
+import pyspark.sql.functions as psf
+
 lines = spark.read.text("gutenberg")
 #lines.show()
 
-words = lines.select("value")
+words = lines.select(psf.split("value","\s"))
 words.show()
