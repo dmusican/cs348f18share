58fc9d3680733127968a4dbbe311f81c0005d09b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-11-05 14:30:48 -0600

    Scripted auto-commit on change (2018-11-05 14:30:48) by gitwatch.sh

--- spark/wordcount2.py
+++ spark/wordcount2.py
@@ -4,8 +4,9 @@ import pyspark.sql.functions as psf
 lines = spark.read.text("/Accounts/dmusicant/Downloads/gutenberg/gutenberg/*/b*")
 #lines.show()
 
-words1 = lines.select(psf.split("value","\s")) \
-              .toDF("wordlist")
+words1 = lines.select(psf.input_file_name(), \
+                      psf.split("value","\s")) \
+              .toDF("filename", "wordlist")
 
 words2 = words1.select(psf.explode("wordlist")) \
               .toDF("word")
