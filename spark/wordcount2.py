48a9735d8845d4afc74d76f24d9143d833c529e6
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-11-05 14:29:38 -0600

    Scripted auto-commit on change (2018-11-05 14:29:38) by gitwatch.sh

---
+++ spark/wordcount2.py
@@ -0,0 +1,19 @@
+import pyspark.sql.functions as psf
+
+#lines = spark.read.text("gutenberg")
+lines = spark.read.text("/Accounts/dmusicant/Downloads/gutenberg/gutenberg/*/b*")
+#lines.show()
+
+words1 = lines.select(psf.split("value","\s")) \
+              .toDF("wordlist")
+
+words2 = words1.select(psf.explode("wordlist")) \
+              .toDF("word")
+
+results = words2.groupBy("word") \
+                .agg(psf.count("word")) \
+                .toDF("word","thecount")
+
+#results.show()
+
+results.repartition(1).write.save("output", format="csv")
