dafc3a2a590d2cc40530cf5d36ddfbe34d974d05
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-11-07 13:57:59 -0600

    Scripted auto-commit on change (2018-11-07 13:57:59) by gitwatch.sh

--- spark/dojson.py
+++ spark/dojson.py
@@ -3,31 +3,6 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("wordcount").getOrCreate()
 
-#lines = spark.read.text("gutenberg")
-lines = spark.read.text("/Accounts/dmusicant/Downloads/gutenberg/gutenberg/*/b*")
-#lines.show()
+mydata = spark.read.json("/Accounts/dmusicant/currentwork/cs348f18share/spark/jsonstuff.log")
 
-words1 = lines.select(psf.input_file_name(), \
-                      psf.split("value","\s")) \
-              .toDF("filename", "wordlist")
-
-words1a = words1.select(\
-        psf.split("filename","/"), "wordlist") \
-           .toDF("filenamesplit", "wordlist")
-
-words1b = words1a.select(\
-            words1a.filenamesplit[ \
-            psf.size(words1a.filenamesplit)-1] \
-                         , "wordlist") \
-           .toDF("realfilename", "wordlist")
-
-words2 = words1b.select("realfilename",psf.explode("wordlist")) \
-              .toDF("realfilename","word")
-
-results = words2.groupBy("realfilename","word") \
-                .agg(psf.count("word")) \
-                .toDF("filename","word","thecount")
-
-#results.show()
-
-results.repartition(1).write.save("output", format="csv")
+mydata.show()
